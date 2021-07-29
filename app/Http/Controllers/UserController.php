6cb5d659d7fd0c32a57c4e0b00dfb07aa0106177
kitanoshohei <kitano.1105.shohei2598@gmail.com>
2021-07-29 23:11:39 +0900

    7/29 ログイン、登録機能API実装完了

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    //
+    public function login(Request $request) {
+        return User::where('user_id', $request->user_id)
+            ->where('password', $request->password)->first();
+    }
+
+    public function register(Request $request) {
+        $user = new User();
+        $user->user_name = $request->user_name;
+        $user->mail_address = $request->mail_address;
+        $user->password = $request->password;
+        $user->job = $request->job_id;
+        $user->profile = $request->profile;
+        $result = $user->save();
+        return [
+            'user_name' => $user->user_name,
+        ];
+    }
 }
--- app/User.php
+++ app/User.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     //
+    const CREATED_AT = 'create_at';
+    const UPDATED_AT = 'update_at';
 }
